4775317a5d00f5edbb99135fb3d72169a2b73c93
Doan Hoang Huy <92454872+HwiHwy@users.noreply.github.com>
2023-06-20 04:11:22 +0700

    contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,9 +1,112 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react';
+import PropTypes from 'prop-types';
+import './contact.scss';
 
-const ContactPage = () => {
+const ContactPage = ({ onSubmit }) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleFirstNameChange = (e) => {
+        setFirstName(e.target.value);
+    };
+
+    const handleLastNameChange = (e) => {
+        setLastName(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleSubjectChange = (e) => {
+        setSubject(e.target.value);
+    };
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Pass the form data to the onSubmit function
+        onSubmit({ firstName, lastName, email, subject, message });
+
+        // Reset form fields
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setSubject('');
+        setMessage('');
+    };
+
     return (
-        <div>ContactPage</div>
-    )
-}
+        <div className="contact-container">
+            <form onSubmit={handleSubmit}>
+                <div className="name-group">
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            value={firstName}
+                            onChange={handleFirstNameChange}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input
+                            type="text"
+                            id="lastName"
+                            value={lastName}
+                            onChange={handleLastNameChange}
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="subject">Subject:</label>
+                    <input
+                        type="text"
+                        id="subject"
+                        value={subject}
+                        onChange={handleSubjectChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                        id="message"
+                        value={message}
+                        onChange={handleMessageChange}
+                        required
+                        className="textarea-fixed-height" 
+                    />
 
-export default memo(ContactPage)
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+ContactPage.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+
+export default memo(ContactPage);
