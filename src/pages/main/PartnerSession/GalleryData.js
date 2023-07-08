986e33e09396f2cd2eed0371f11a54f06149fba3
Doan Hoang Huy <92454872+HwiHwy@users.noreply.github.com>
2023-07-09 00:55:26 +0700

    donedata

--- src/pages/main/PartnerSession/GalleryData.js
+++ src/pages/main/PartnerSession/GalleryData.js
@@ -1,212 +1,340 @@
 export const GalleryData = [
-    {
-        id:1,
-        titile:"travel",
-        image:"./images/tv_khoailangthang.jpg",
-        name: "Khoai Lang Thang"
-    },
-    {
-        id:2,
-        titile:"travel",
-        image:"./images/tv_LyThanhCo.jpg",
-        name: "Ly Thanh Co"
+  {
+    id: 1,
+    titile: "travel",
+    image: "./images/tv_khoailangthang.jpg",
+    name: "Khoai Lang Thang",
+  },
+  {
+    id: 2,
+    titile: "travel",
+    image: "./images/tv_LyThanhCo.jpg",
+    name: "Ly Thanh Co",
+  },
+  {
+    id: 3,
+    titile: "travel",
+    image: "./images/tv_PhanTheAnh.jpg",
+    name: "Phan The Anh",
+  },
+  {
+    id: 4,
+    titile: "travel",
+    image: "./images/tv_Travip.jpg",
+    name: "Travip",
+  },
+  {
+    id: 5,
+    titile: "travel",
+    image: "./images/tv_truongthanhduy.jpg",
+    name: "Truong Thanh Duy",
+  },
+  {
+    id: 6,
+    titile: "travel",
+    image: "./images/tv_hukha.jpg",
+    name: "HuKhaaa",
+  },
+  {
+    id: 7,
+    titile: "travel",
+    image: "./images/tv_buuvivu.jpg",
+    name: "Buu Vi Vu",
+  },
+  {
+    id: 8,
+    titile: "travel",
+    image: "./images/tv_kephatdat.jpg",
+    name: "Ke Phat Dat",
+  },
+  {
+    id: 9,
+    titile: "travel",
+    image: "./images/tv_ToTonThanh.jpg",
+    name: "To Ton Thanh",
+  },
+  {
+    id: 10,
+    titile: "travel",
+    image: "./images/bt_hahien.jpg",
+    name: "Ha Hien",
+  },
 
-        
-    },
-    {
-        id:3,
-        titile:"travel",
-        image:"./images/tv_PhanTheAnh.jpg",
-        name: "Phan The Anh"
+  //Beauty
 
-    },
-    {
-        id:4,
-        titile:"travel",
-        image:"./images/tv_Travip.jpg",
-        name: "Travip"
+  {
+    id: 11,
+    titile: "beauty",
+    image: "./images/bt_duyvu.jpg",
+    name: "Vu Duy",
+  },
+  {
+    id: 12,
+    titile: "beauty",
+    image: "./images/bt_hoanglong.png",
+    name: "Hoang Long",
+  },
+  {
+    id: 13,
+    titile: "beauty",
+    image: "./images/bt_kimanh.jpg",
+    name: "Kim Anh",
+  },
+  {
+    id: 14,
+    titile: "beauty",
+    image: "./images/bt_tutran.jpg",
+    name: "Tu Tran",
+  },
+  {
+    id: 155,
+    titile: "beauty",
+    image: "./images/bt_tuyetly.jpg",
+    name: "Tuyet Ly",
+  },
+  {
+    id: 15,
+    titile: "beauty",
+    image: "./images/bt_thaonhile.jpg",
+    name: "Thao Nhi Le",
+  },
+  {
+    id: 16,
+    titile: "beauty",
+    image: "./images/bt_Halinh.jpg",
+    name: "Ha Linh",
+  },
 
-    },
-    {
-        id:5,
-        titile:"travel",
-        image:"./images/tv_truongthanhduy.jpg",
-        name: "Truong Thanh Duy"
+  {
+    id: 17,
+    titile: "beauty",
+    image: "./images/bt_thudannguyen.jpg",
+    name: "Thu Dan Nguyen",
+  },
+  {
+    id: 18,
+    titile: "beauty",
+    image: "./images/bt_chloenguyen.jpg",
+    name: "Chloe Nguyen",
+  },
+  {
+    id: 19,
+    titile: "beauty",
+    image: "./images/bt_huongwitch.jpg",
+    name: "Huong Witch",
+  },
+  {
+    id: 20,
+    titile: "beauty",
+    image: "./images/bt_lintieu.jpg",
+    name: "Lin Tieu",
+  },
+  {
+    id: 21,
+    titile: "beauty",
+    image: "./images/bt_yendan.jpg",
+    name: "Yen Dan",
+  },
+  {
+    id: 22,
+    titile: "beauty",
+    image: "./images/bt_chanhthu.jpg",
+    name: "Chanh Thu",
+  },
+  {
+    id: 23,
+    titile: "beauty",
+    image: "./images/bt_hieubm.jpg",
+    name: "Hieu BM",
+  },
+  {
+    id: 24,
+    titile: "beauty",
+    image: "./images/bt_rontran.jpg",
+    name: "Ron Tran",
+  },
+  {
+    id: 25,
+    titile: "beauty",
+    image: "./images/bt_tramxing.jpg",
+    name: "Tramxing",
+  },
 
-    },
-    {
-        id:6,
-        titile:"travel",
-        image:"./images/tv_hukha.jpg",
-        name: "HuKhaaa"
+  //Fashion
+  {
+    id: 26,
+    titile: "fashion",
+    image: "./images/fs_sonpham.jpg",
+    name: "Son Pham",
+  },
+  {
+    id: 27,
+    titile: "fashion",
+    image: "./images/fs_max.jpg",
+    name: "Max",
+  },
+  {
+    id: 28,
+    titile: "fashion",
+    image: "./images/fs_lam.jpg",
+    name: "Lam",
+  },
+  {
+    id: 29,
+    titile: "fashion",
+    image: "./images/fs_khangtran.jpg",
+    name: "Khang Tran",
+  },
+  {
+    id: 30,
+    titile: "fashion",
+    image: "./images/fs_lyly.jpg",
+    name: "LyLy",
+  },
+  {
+    id: 31,
+    titile: "fashion",
+    image: "./images/fs_caophat.jpg",
+    name: "Cao Phat",
+  },
+  {
+    id: 32,
+    titile: "fashion",
+    image: "./images/fs_huutri.jpg",
+    name: "Huu Tri",
+  },
+  {
+    id: 33,
+    titile: "fashion",
+    image: "./images/fs_banni.jpg",
+    name: "Banni Pham",
+  },
+  {
+    id: 34,
+    titile: "fashion",
+    image: "./images/fs_chaubui.jpg",
+    name: "Chau Bui",
+  },
+  {
+    id: 35,
+    titile: "fashion",
+    image: "./images/fs_chuhoaibao.jpg",
+    name: "Chu Hoai Bao",
+  },
+  {
+    id: 36,
+    titile: "fashion",
+    image: "./images/fs_emma.jpg",
+    name: "Emma",
+  },
+  {
+    id: 37,
+    titile: "fashion",
+    image: "./images/fs_kris.jpg",
+    name: "Kris Nhi",
+  },
+  {
+    id: 38,
+    titile: "fashion",
+    image: "./images/fs_levi.jpg",
+    name: "Levi",
+  },
+  {
+    id: 39,
+    titile: "fashion",
+    image: "./images/fs_lukeD.jpg",
+    name: "LukeD",
+  },
+  {
+    id: 40,
+    titile: "fashion",
+    image: "./images/fs_minhkha.jpg",
+    name: "Minh Kha",
+  },
+  {
+    id: 41,
+    titile: "fashion",
+    image: "./images/fs_minhsang.jpg",
+    name: "Minh Sang",
+  },
+  {
+    id: 42,
+    titile: "fashion",
+    image: "./images/fs_miwan.jpg",
+    name: "Miwan",
+  },
+  {
+    id: 43,
+    titile: "fashion",
+    image: "./images/fs_vinhdam.jpg",
+    name: "Vinh Dam",
+  },
+  // Freestyle
 
-    },
-    {
-        id:7,
-        titile:"travel",
-        image:"./images/tv_buuvivu.jpg",
-        name: "Buu Vi Vu"
-
-    },
-    {
-        id:8,
-        titile:"travel",
-        image:"./images/tv_kephatdat.jpg",
-        name: "Ke Phat Dat"
-
-    },
-//Beauty
-
-
-    {
-        id:9,
-        titile:"beauty",
-        image:"./images/bt_thaonhile.jpg"
-    },
-    {
-        id:10,
-        titile:"beauty",
-        image:"./images/bt_Halinh.jpg"
-    },
-
-    {
-        id:11,
-        titile:"beauty",
-        image:"./images/bt_thudannguyen.jpg"
-    },
-    {
-        id:12,
-        titile:"beauty",
-        image:"./images/bt_chloenguyen.jpg"
-    },
-    {
-        id:13,
-        titile:"beauty",
-        image:"./images/bt_huongwitch.jpg"
-    },
-    {
-        id:14,
-        titile:"beauty",
-        image:"./images/bt_lintieu.jpg"
-    },
-    {
-        id:15,
-        titile:"beauty",
-        image:"./images/bt_yendan.jpg"
-    },
-    {
-        id:16,
-        titile:"beauty",
-        image:"./images/bt_chanhthu.jpg"
-    },
-    {
-        id:17,
-        titile:"beauty",
-        image:"./images/bt_hieubm.jpg"
-    },
-    {
-        id:18,
-        titile:"beauty",
-        image:"./images/bt_rontran.jpg"
-    },
-    {
-        id:19,
-        titile:"beauty",
-        image:"./images/bt_tramxing.jpg"
-    },
-
-//Fashion
-    {
-        id:20,
-        titile:"fashion",
-        image:"./images/fs_banni.jpg"
-    },
-    {
-        id:21,
-        titile:"fashion",
-        image:"./images/fs_chaubui.jpg"
-    },
-    {
-        id:22,
-        titile:"fashion",
-        image:"./images/fs_chuhoaibao.jpg"
-    },
-    {
-        id:23,
-        titile:"fashion",
-        image:"./images/fs_emma.jpg"
-    },
-    {
-        id:24,
-        titile:"fashion",
-        image:"./images/fs_kris.jpg"
-    },
-    {
-        id:25,
-        titile:"fashion",
-        image:"./images/fs_levi.jpg"
-    },
-    {
-        id:26,
-        titile:"fashion",
-        image:"./images/fs_lukeD.jpg"
-    },
-    {
-        id:27,
-        titile:"fashion",
-        image:"./images/fs_minhkha.jpg"
-    },
-    {
-        id:28,
-        titile:"fashion",
-        image:"./images/fs_minhsang.jpg"
-    },
-    {
-        id:29,
-        titile:"fashion",
-        image:"./images/fs_miwan.jpg"
-    },
-    {
-        id:30,
-        titile:"fashion",
-        image:"./images/fs_vinhdam.jpg"
-    },
-// Freestyle
-
-    {
-        id:31,
-        titile:"FreeStyle   ",
-        image:"./images/fe_duongvu.jpg"
-    },
-    {
-        id:32,
-        titile:"FreeStyle   ",
-        image:"./images/fe_feifei.jpg"
-    },
-    {
-        id:33,
-        titile:"FreeStyle   ",
-        image:"./images/fe_huynhkhanhdong.jpg"
-    },
-    {
-        id:34,
-        titile:"FreeStyle   ",
-        image:"./images/fe_naky.jpg"
-    },
-    {
-        id:35,
-        titile:"FreeStyle   ",
-        image:"./images/fe_nhatquang.jpg"
-    },
-    {
-        id:36,
-        titile:"FreeStyle   ",
-        image:"./images/fe_phamthebang.jpg"
-    },
-    
-
-
-
-
-
-]
+  {
+    id: 44,
+    titile: "FreeStyle   ",
+    image: "./images/fe_duongvu.jpg",
+    name: "Duong Vu",
+  },
+  {
+    id: 45,
+    titile: "FreeStyle   ",
+    image: "./images/fe_feifei.jpg",
+    name: "Fei Fei",
+  },
+  {
+    id: 46,
+    titile: "FreeStyle   ",
+    image: "./images/fe_huynhkhanhdong.jpg",
+    name: "Huynh Khanh Dong",
+  },
+  {
+    id: 47,
+    titile: "FreeStyle   ",
+    image: "./images/fe_naky.jpg",
+    name: "Naky",
+  },
+  {
+    id: 48,
+    titile: "FreeStyle   ",
+    image: "./images/fe_nhatquang.jpg",
+    name: "Nhat Quang",
+  },
+  {
+    id: 49,
+    titile: "FreeStyle   ",
+    image: "./images/fe_phamthebang.jpg",
+    name: "Pham The Bang",
+  },
+  {
+    id: 50,
+    titile: "FreeStyle   ",
+    image: "./images/fe_tutrung.jpg",
+    name: "Tu Trung",
+  },
+  {
+    id: 51,
+    titile: "FreeStyle   ",
+    image: "./images/fe_truclinh.jpg",
+    name: "Truc Linh",
+  },
+  {
+    id: 52,
+    titile: "FreeStyle   ",
+    image: "./images/fe_thuyquynh.jpg",
+    name: "Thuy Quynh",
+  },
+  {
+    id: 53,
+    titile: "FreeStyle   ",
+    image: "./images/fe_minhtriet.jpg",
+    name: "Minh Triet",
+  },
+  {
+    id: 54,
+    titile: "FreeStyle   ",
+    image: "./images/fe_anhthu.jpg",
+    name: "Anh Thu",
+  },
+];
