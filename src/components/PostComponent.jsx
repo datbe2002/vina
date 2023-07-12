d89e4d2cfad6392b7cd374671eb9b7e8babe61b4
Doan Hoang Huy <92454872+HwiHwy@users.noreply.github.com>
2023-07-12 23:35:01 +0700

    fiximg

--- src/components/PostComponent.jsx
+++ src/components/PostComponent.jsx
@@ -17,7 +17,7 @@ const PostComponent = () => {
                 <div key={blog._id}>
                     <Link to={`/blog/${blog._id}`} className='post'>
 
-                        <img src={URL + blog.cover} alt="hinh" />
+                        <img src={URL + blog.cover} alt="hinh" className='imgBlog'/>
                         <div className='post-content'>
                             <h2 className='post-title'>{blog.title}</h2>
                             <p className='info'>
